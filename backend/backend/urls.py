174538f8531f30b24de526a6097d94ae1fbefe60
Vedant Lahane <vedantanillahane@gmail.com>
2025-12-01 00:48:37 +0530

    Implement code changes to enhance functionality and improve performance

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,8 +2,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 from .views import home_view
 
@@ -14,5 +15,9 @@ urlpatterns = [
     path('api/', include('agent.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files 
+# In production, media files should be served by a web server (nginx, etc.)
+# But for local development/testing, Django can serve them
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
